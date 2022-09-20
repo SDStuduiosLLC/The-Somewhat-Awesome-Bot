ab6a112e97ab9b9d44ee2120021b03840d913c0f
summerisadev <101482788+summerisadev@users.noreply.github.com>
2022-09-20 18:20:00 +0100

    Removed unused files

--- src/Command.ts
+++
@@ -1,5 +0,0 @@
-import {ChatInputApplicationCommandData, Client, CommandInteraction} from "discord.js";
-
-export interface Command extends ChatInputApplicationCommandData {
-    run: (client: Client, ctx: CommandInteraction) => void;
-};
