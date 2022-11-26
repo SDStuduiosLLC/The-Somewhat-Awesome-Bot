0629e19cb36d78c600fe2d9b45cfa076eb0daa44
summerisadev <101482788+summerisadev@users.noreply.github.com>
2022-11-26 17:12:03 +0000

    Added custom logger

---
+++ src/lib/tsabLogger.ts
@@ -0,0 +1,184 @@
+import "chalk"
+import "moment"
+import { config } from "../../data/config"
+import moment from "moment"
+import chalk from "chalk"
+import fs from "fs"
+import { EmbedBuilder, WebhookClient } from "discord.js"
+
+/**
+ * Checks if a log file exists. Returns the path, and creates one if required.
+ * @return {string} filePath
+ */
+function getLogFile() {
+  let filePath = ""
+
+  if (
+    !config.tsabLoggerSetting.logFilePath ||
+    config.tsabLoggerSetting.logFilePath === undefined
+  ) {
+    filePath = "data/tsab.log"
+  } else {
+    filePath = config.tsabLoggerSetting.logFilePath
+  }
+
+  if (!fs.existsSync(`${filePath}`)) {
+    fs.writeFile(
+      filePath,
+      "TSAB LOGGER.\nAUTOMATICALLY GENERATED FILE. DO NOT EDIT OR DELETE.",
+      (err) => {
+        if (err) throw err
+        debug("Created TSAB log file")
+      }
+    )
+  }
+
+  return filePath
+}
+
+getLogFile()
+
+/**
+ * Template for logger level options. You can ignore this.
+ * @param {boolean} sendWebhook - Default: true
+ */
+interface OptionsInt {
+  sendWebhook?: boolean
+}
+
+/**
+ * @param {string} message
+ * @param {OptionsInt} options
+ */
+export async function debug(message: string, options?: OptionsInt) {
+  const sendWebhook = options?.sendWebhook
+  const localmessage = `${moment().format()} ${chalk.grey(
+    "DEBUG"
+  )} | ${chalk.white(message)}\n`
+
+  process.stdout.write(`${localmessage}`)
+  fs.appendFile(getLogFile(), localmessage, (err) => console.error(err))
+
+  if (sendWebhook) {
+    webhookReporter("debug", message)
+  }
+}
+
+/**
+ * @param {string} message
+ * @param {OptionsInt} options
+ */
+export async function log(message: string, options?: OptionsInt) {
+  const sendWebhook = options?.sendWebhook
+  const localmessage = `${moment().format()} ${chalk.green(
+    "LOG"
+  )} | ${chalk.white(message)}\n`
+
+  process.stdout.write(`${localmessage}`)
+  fs.appendFile(getLogFile(), localmessage, (err) => console.error(err))
+
+  if (sendWebhook) {
+    webhookReporter("info", message)
+  }
+}
+
+/**
+ * @param {string} message
+ * @param {OptionsInt} options
+ */
+export async function warn(message: string, options?: OptionsInt) {
+  const sendWebhook = options?.sendWebhook
+  const localmessage = `${moment().format()} ${chalk.yellow(
+    "WARN"
+  )} | ${chalk.white(message)}\n`
+
+  process.stdout.write(`${localmessage}`)
+  fs.appendFile(getLogFile(), localmessage, (err) => console.error(err))
+
+  if (sendWebhook) {
+    webhookReporter("warn", message)
+  }
+}
+
+/**
+ * @param {string} message
+ * @param {OptionsInt} options
+ */
+export async function error(message: string, options?: OptionsInt) {
+  const sendWebhook = options?.sendWebhook
+  const localmessage = `${moment().format()} ${chalk.redBright(
+    "ERROR"
+  )} | ${chalk.grey(message)}\n`
+
+  process.stdout.write(`${localmessage}`)
+  fs.appendFile(getLogFile(), localmessage, (err) => console.error(err))
+
+  if (sendWebhook) {
+    webhookReporter("error", message)
+  }
+}
+
+async function webhookReporter(mode: string, text: string) {
+  if (!config.tsabLoggerSetting.loggingWebhook) {
+    return console.error("No webhook found! Check your config!")
+  }
+
+  const whClient = new WebhookClient({
+    url: config.tsabLoggerSetting.loggingWebhook,
+  })
+
+  switch (mode) {
+    case "debug":
+      const debugEmbed = new EmbedBuilder()
+        .setTitle("Debug Log")
+        .setDescription(text)
+        .setColor("Blue")
+        .setTimestamp()
+        .setFooter({ text: "TSAB Utilities" })
+
+      return await whClient.send({ embeds: [debugEmbed] })
+
+    case "info":
+      const infoEmbed = new EmbedBuilder()
+        .setTitle("Log Line")
+        .setDescription(text)
+        .setColor("#2f3136")
+        .setTimestamp()
+        .setFooter({ text: "TSAB Utilities" })
+
+      return await whClient.send({ embeds: [infoEmbed] })
+
+    case "warn":
+      const warnEmbed = new EmbedBuilder()
+        .setTitle("Warn Log")
+        .setDescription(text)
+        .setColor("Yellow")
+        .setTimestamp()
+        .setFooter({ text: "TSAB Utilities" })
+
+      return await whClient.send({ embeds: [warnEmbed] })
+
+    case "error":
+      const errorEmbed = new EmbedBuilder()
+        .setTitle("Error Log")
+        .setDescription(text)
+        .setColor("Red")
+        .setTimestamp()
+        .setFooter({ text: "TSAB Utilities" })
+
+      return await whClient.send({ embeds: [errorEmbed] })
+
+    case "note":
+      const noteEmbed = new EmbedBuilder()
+        .setTitle("Note")
+        .setDescription(text)
+        .setColor("Green")
+        .setTimestamp()
+        .setFooter({ text: "TSAB Utilities" })
+
+      return await whClient.send({ embeds: [noteEmbed] })
+
+    default:
+      return console.warn("Not a valid log type!")
+  }
+}
